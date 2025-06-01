22c1eba3285885ef9ee0b7fbfb78fa5e56bbf01a
lc3lx <140909706+lc3lx@users.noreply.github.com>
2025-06-02 00:19:05 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def fetch_email_with_link(account, subject_keywords, button_text):
     try:
         mail.select("inbox")
         _, data = mail.search(None, 'ALL')
-        mail_ids = data[0].split()[-100:]  # Increased to last 100 emails
+        mail_ids = data[0].split()[-20:]  # Increased to last 100 emails
         for mail_id in reversed(mail_ids):
             _, msg_data = mail.fetch(mail_id, "(RFC822)")
             raw_email = msg_data[0][1]
@@ -135,7 +135,7 @@ def fetch_email_with_code(account, subject_keywords):
     try:
         mail.select("inbox")
         _, data = mail.search(None, 'ALL')
-        mail_ids = data[0].split()[-100:]  # Increased to last 100 emails
+        mail_ids = data[0].split()[-20:]  # Increased to last 100 emails
         for mail_id in reversed(mail_ids):
             _, msg_data = mail.fetch(mail_id, "(RFC822)")
             raw_email = msg_data[0][1]
